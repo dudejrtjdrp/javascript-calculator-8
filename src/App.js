b90e10ee763d3c571a2bfe9b7356edb14b6f9526
dudejrtjdrp <dudejrtjdrp@naver.com>
2025-10-14 18:50:11 +0900

    feat(app): InputHandler와 OutputHandler 연결 및 실행 흐름 구현
    App 클래스에서 InputHandler를 통해 입력 받고 OutputHandler로 출력 처리
    try-catch 구문으로 에러 발생 시 printError 메서드 호출

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import InputHandler from "./view/inputHandler";
+import OutputHandler from "./view/outputHandler";
 
 class App {
   async run() {
+    try {
+      const input = await InputHandler.read();
+      OutputHandler.print(input);
+    } catch (error) {
+      OutputHandler.printError(error);
+    }
   }
 }
 
